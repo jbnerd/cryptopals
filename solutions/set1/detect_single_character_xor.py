983017629c9601842aadb59d51d88abe31836f3d
jbnerd <abhivjoshi.aj@gmail.com>
2020-05-09 18:19:45 +0530

    Add solution to set1 problem4

---
+++ solutions/set1/detect_single_character_xor.py
@@ -0,0 +1,26 @@
+from lib.cipher_utils.deciphers import SingleByteEnglishXor
+from lib.core.base_converters import Converter
+
+
+def read_file():
+    file_name = 'solutions/set1/data/detect_single_character_xor.in'
+    with open(file_name, 'r') as infile:
+        lines = infile.read().split('\n')[:-1]
+    return lines
+
+
+def main():
+    decipherer = SingleByteEnglishXor()
+    lines = read_file()
+    max_score, best_deciphered_string = 0, None
+    for line in lines:
+        binary_string = Converter.convert(line, '16', '2')
+        deciphered_string, score = decipherer.decode(binary_string)
+        if score > max_score:
+            max_score = score
+            best_deciphered_string = deciphered_string
+    print(best_deciphered_string)
+
+
+if __name__ == "__main__":
+    main()
